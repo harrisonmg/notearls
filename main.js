00a0f27c0444d2db102bb333196417e55029816b
Harrison MG <gieraltowski.har@gmail.com>
2022-12-05 00:40:12 -0500

    init

---
+++ main.js
@@ -0,0 +1,48 @@
+const fractionRegex = /(\d+)\/(\d+)/
+
+const addDrink = (section, name, content) => {
+  const drink = document.createElement('div');
+  drink.classList.add('drink');
+
+  const drinkName = document.createElement('p');
+  drinkName.classList.add('drink-name');
+  drinkName.textContent = name;
+  drink.appendChild(drinkName);
+
+  const table = document.createElement('table');
+  for (const item of content) {
+    const row = document.createElement('tr');
+
+    const amount = document.createElement('td');
+    let amountString = "";
+    if (item.length > 1) {
+      amountString = item[0];
+      const match = amountString.match(fractionRegex);
+      if (match !== null) {
+        amountString = amountString.replace(fractionRegex, '');
+        amountString += `<sup class="frac">${match[1]}</sup>&frasl;<span class="frac">${match[2]}</span>`;
+      }
+    }
+    amount.innerHTML = amountString;
+    row.appendChild(amount);
+
+    const ingredient = document.createElement('td');
+    ingredient.innerText = item.pop();
+    row.appendChild(ingredient);
+
+    table.appendChild(row);
+  }
+  drink.appendChild(table);
+
+  document.getElementById(section).appendChild(drink);
+}
+
+fetch('./drinks.json')
+    .then((response) => response.json())
+    .then((drinks) => {
+      for (const section of Object.keys(drinks)) {
+        for (const drink of drinks[section]) {
+          addDrink(section, drink[0], drink.slice(1));
+        }
+      }
+    });
